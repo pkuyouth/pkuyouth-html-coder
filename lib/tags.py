14c0f07d61faf66c7ef9ff6b88565e9177bd73ba
debian-9 <zxh2017@pku.edu.cn>
2018-10-10 23:41:17 +0800

    v1.0.9 update 181010

--- lib/tags.py
+++ lib/tags.py
@@ -3,7 +3,7 @@
 # filename: lib/tags.py
 #
 # 自定义的 HTML 节点类
-#
+
 
 from lxml import etree
 from lxml.builder import E
@@ -18,7 +18,8 @@ __all__ = [
     'span',\
     'Br','Hr','Img','H1','R15','R16','NSyb',\
     'P','PRNote','ImgNote','PEndNote','PCount','PRpt','PRef','PHr',\
-    'HeadBox','BodyBox','TailBox','EdtNoteBox','RptNoteBox','CountBox','RefBox','WrapBox',\
+    'HeadBox','BodyBox','EndingBox','CountBox','WrapBox',\
+        'ReporterBox','EditorNoteBox','ReporterNoteBox','ReferenceBox',\
     'HTML',\
 ]
 
@@ -117,7 +118,8 @@ class Tag(Node):
             for child in args:
                 self.append(child, bold=bold)
 
-    def __to_child_node(self, child, *args, bold=False):
+    @staticmethod
+    def __to_child_node(child, *args, bold=False):
         """ 将 __init__ 传入节点解析为 Node 节点
 
             Args:
@@ -134,10 +136,15 @@ class Tag(Node):
         else:
             raise TypeError("children of Tag must be a (str,int,float) or Node type, not %s !" % type(child))
 
+    def __len__(self):
+        """ 子节点个数
+        """
+        return len(self.children)
+
     def has_child(self):
         """ 是否拥有子节点
         """
-        return len(self.children) > 0
+        return len(self) > 0
 
     def insert(self, *args, index=0, bold=False):
         """ 类似于 list.insert ，在 children 列表中特定位置顺序添加若干子节点
@@ -344,17 +351,20 @@ class HeadBox(section):
 class BodyBox(section):
     class_ = 'div-body'
 
-class TailBox(section):
-    class_ = 'div-tail'
+class EndingBox(section):
+    class_ = 'div-ending'
 
-class EdtNoteBox(section):
-    class_ = 'div-edtnote'
+class ReporterBox(section):
+    class_ = 'div-reporter'
 
-class RptNoteBox(section):
-    class_ = 'div-rptnote'
+class EditorNoteBox(section):
+    class_ = 'div-editornote'
 
-class RefBox(section):
-    class_ = 'div-ref'
+class ReporterNoteBox(section):
+    class_ = 'div-reporternote'
+
+class ReferenceBox(section):
+    class_ = 'div-reference'
 
 class CountBox(section):
     class_ = 'div-count'
--- project/main.py
+++ project/main.py
@@ -4,8 +4,8 @@
 
 
 __author__  = "Rabbit"
-__version__ = "1.0.8"
-__date__    = "2018.10.05"
+__version__ = "1.0.9"
+__date__    = "2018.10.10"
 
 
 import sys
@@ -29,66 +29,62 @@ Build_Dir = os.path.join(Project_Dir, 'build/')             # 输出文件夹
 
 
 def __get_docx_file(folder=Project_Dir):
-	""" 返回目标文件夹下修改日期最新的 docx 文件路径
+    """ 返回目标文件夹下修改日期最新的 docx 文件路径
 
-		Args:
-			folder    str    目标文件夹路径，此处即为工程文件夹路径
-		Returns:
-			file      str    docx 文件路径
-		Raises:
-			NoDocxFileError  未找到任何 docx 文件
-	"""
-	for file in sorted(os.listdir(folder), key=lambda file: os.path.getmtime(file), reverse=True):
-		if not os.path.isdir(file) and os.path.splitext(file)[1] == '.docx':
-			return file
-		else:
-			continue
-	raise NoDocxFileError('*.docx file is missing in %s !' % os.path.abspath(folder))
+        Args:
+            folder    str    目标文件夹路径，此处即为工程文件夹路径
+        Returns:
+            file      str    docx 文件路径
+        Raises:
+            NoDocxFileError  未找到任何 docx 文件
+    """
+    for file in sorted(os.listdir(folder), key=lambda file: os.path.getmtime(file), reverse=True):
+        if not os.path.isdir(file) and os.path.splitext(file)[1] == '.docx':
+            return file
+        else:
+            continue
+    raise NoDocxFileError('*.docx file is missing in %s !' % os.path.abspath(folder))
 
 
 def main(**kwargs):
 
-	htmlcoder = HTMLCoder(file=__get_docx_file(), output=Build_Dir, **kwargs)
-	htmlcoder.work()
+    htmlcoder = HTMLCoder(file=__get_docx_file(), output=Build_Dir, **kwargs)
+    htmlcoder.work()
 
-	with open(os.path.join(Static_Dir, "preview.template.html"), "r", encoding="utf-8-sig") as rfp:
-		with open(os.path.join(Build_Dir, "preview.html"), "w", encoding="utf-8") as wfp:
-			wfp.write(rfp.read().format(
-				title = htmlcoder.filename,
-				src = "./%s.html" % htmlcoder.filename,
-				timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),
-			))
+    with open(os.path.join(Static_Dir, "preview.template.html"), "r", encoding="utf-8-sig") as rfp:
+        with open(os.path.join(Build_Dir, "preview.html"), "w", encoding="utf-8") as wfp:
+            wfp.write(rfp.read().format(
+                title = htmlcoder.filename,
+                src = "./%s.html" % htmlcoder.filename,
+                timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),
+            ))
 
 
 if __name__ == '__main__':
 
-	parser = OptionParser(
-		version="HTMLCoder %s" % __version__,
-		description="HTMLCoder -- A small tool that can convert a '*.docx' file to a '*.html' file, which is in accord with PKUyouth's style.")
+    parser = OptionParser(
+        version="HTMLCoder %s" % __version__,
+        description="HTMLCoder -- A small tool that can convert a '*.docx' file to a '*.html' file, which is in accord with PKUyouth's style.")
 
-	group_base = OptionGroup(parser,
-			title="Base Options")
-	group_base.add_option("-s", "--static", dest="static_server_type", metavar="TYPE",
-								help="Type of static server. Options: ['Tietuku','SM.MS','Elimage']")
+    group_base = OptionGroup(parser,
+            title="Base Options")
+    group_base.add_option("-s", "--static", dest="static_server_type", metavar="TYPE",
+                                help="Type of static server. Options: ['Tietuku','SM.MS','Elimage']")
 
-	group_coding_params = OptionGroup(parser,
-			title="Parameters of Coding Process",
-			description="Or will use default setting from 'config/coder.ini' file.")
-	group_coding_params.add_option("--no-reporter", action="store_true", dest="no_reporter",
-										help="Whether this article has reporters information or not. (Default: False)")
-	group_coding_params.add_option("--has-reference", action="store_true", dest="has_reference",
-										help="Whether this article has references or not. (Default: False)")
-	group_coding_params.add_option("--count-picture", action="store_true", dest="count_picture",
-										help="Output picture's sum. (Default: False)")
+    group_coding_params = OptionGroup(parser,
+            title="Parameters of Coding Process",
+            description="Or will use default setting from 'config/coder.ini' file.")
+    group_coding_params.add_option("--count-picture", action="store_true", dest="count_picture",
+                                        help="Output picture's sum. (Default: False)")
 
-	parser.add_option_group(group_base)
-	parser.add_option_group(group_coding_params)
+    parser.add_option_group(group_base)
+    parser.add_option_group(group_coding_params)
 
-	options, args = parser.parse_args()
+    options, args = parser.parse_args()
 
 
-	try:
-		main(**options.__dict__)
-	except Exception as err:
-		logger.exception(err)
-		# raise err
+    try:
+        main(**options.__dict__)
+    except Exception as err:
+        logger.exception(err)
+        # raise err
